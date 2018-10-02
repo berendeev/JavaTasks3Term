678c946f07f9f2a12b4e6c4d1b72d7d51e8dd94c
Берендеев Дмитрий <sellect.47@gmail.com>
2018-10-02 20:41:57 +0600

    make random input and loop

--- src/var3/task2/Main.java
+++ src/var3/task2/Main.java
@@ -2,21 +2,26 @@ package var3.task2;
 
 import javafx.geometry.Point3D;
 
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
-		Vector a = new Vector(new Point3D(1, 2, 3));	// создание векторов для примера
-		Vector b = new Vector(new Point3D(2, 4, 16));
-		Vector c = new Vector(new Point3D(33, 66, 22));
+		for (int i = 0; i < 5; i++) {
+			System.out.println("============= " + (i + 1) + " ===============");
+			Random rnd = new Random();
+			Vector a = new Vector(new Point3D(rnd.nextInt(), rnd.nextInt(), rnd.nextInt()));    // создание векторов для примера
+			Vector b = new Vector(new Point3D(rnd.nextInt(), rnd.nextInt(), rnd.nextInt()));
+			Vector c = new Vector(new Point3D(rnd.nextInt(), rnd.nextInt(), rnd.nextInt()));
 
-		System.out.println("norm for vector a is " + a.norm());	// вывод длины вектора
-		System.out.println("norm for vector b is " + b.norm());
-		System.out.println("norm for vector c is " + c.norm());
+			System.out.println("norm for vector a is " + a.norm());    // вывод длины вектора
+			System.out.println("norm for vector b is " + b.norm());
+			System.out.println("norm for vector c is " + c.norm());
 
-		Vector r = a.addition(b).vectorMultiplication(c); // (a+b) и векторное умножение на c
-		r.info();	// вывод информации о векторе r
+			Vector r = a.addition(b).vectorMultiplication(c); // (a+b) и векторное умножение на c
+			r.info();    // вывод информации о векторе r
 
-		double t = a.addition(b).scalarMultiplication(c); // (a+b)c
-		System.out.println("(a+b)c = " + t);
-
+			double t = a.addition(b).scalarMultiplication(c); // (a+b)c
+			System.out.println("(a+b)c = " + t);
+		}
 	}
 }
