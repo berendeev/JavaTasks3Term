e404f966c792002d07bfec59d3b59a9f73799838
Берендеев Дмитрий <sellect.47@gmail.com>
2018-10-17 23:57:16 +0600

    re rewrite task4

--- src/var8/task4/Main.java
+++ src/var8/task4/Main.java
@@ -30,13 +30,13 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Stack<TextField> first = new Stack<>();	// 1 стэк
-		Stack<TextField> tempStack = new Stack<>();	// временный(3ий)
+		Stack<TextField> first = new Stack<>();    // 1 стэк
+		Stack<TextField> tempStack = new Stack<>();    // временный(3ий)
 		Stack<TextField> second = new Stack<>(); // 2ой
-		for (int i = 0; i < 12; i++) {	// начальные значения
-			first.push(new TextField("A value 1-" + i));
-			second.push(new TextField("B value 2-" + i));
-		}
+//		for (int i = 0; i < 12; i++) {    // начальные значения
+//			first.push(new TextField("A value 1-" + i));
+//			second.push(new TextField("B value 2-" + i));
+//		}
 
 		HBox hBox = new HBox();
 
@@ -60,28 +60,50 @@ public class Main extends Application {
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				if (tempStack.empty()) {	// если временный стэк пуст ( 1 нажатие)
-					while (!first.empty()) {	// пока первый стэк не станет пустым
-						tempStack.push(first.pop());	// перекладывает из первого стэка во временный; pop - получить верхгий(1ый) элемент и удалить из стэка, push затолкнуть в стэк
+				if (tempStack.empty()) {    // если временный стэк пуст ( 1 нажатие)
+					while (!first.empty()) {    // пока первый стэк не станет пустым
+						tempStack.push(first.pop());    // перекладывает из первого стэка во временный; pop - получить верхгий(1ый) элемент и удалить из стэка, push затолкнуть в стэк
 					}
-				} else {	// если во временном стэке чтото есть (2ое нажатие)
-					while (!second.empty()) {	// перекладывает в первый элементы из 2го
+				} else {    // если во временном стэке чтото есть (2ое нажатие)
+					while (!second.empty()) {    // перекладывает в первый элементы из 2го
 						first.push(second.pop());
 					}
-					Collections.reverse(first);	// переворачивает элементы в 1ом стэке
+					Collections.reverse(first);    // переворачивает элементы в 1ом стэке
 
-					while (!tempStack.empty()) {	// перекладывает из временного во 2ой
+					while (!tempStack.empty()) {    // перекладывает из временного во 2ой
 						second.push(tempStack.pop());
 					}
 
 				}
-				firstListView.getItems().setAll(first);	// обновляет данные в ListView
-				tempListView.getItems().setAll(tempStack);
-				secondListView.getItems().setAll(second);
+				update(firstListView, secondListView, tempListView, first, second, tempStack);
 			}
 		});
 
-		borderPane.setBottom(button);
+		Button addRowFirst = new Button("Add row to first list");
+		addRowFirst.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				first.push(new TextField());
+				update(firstListView, secondListView, tempListView, first, second, tempStack);
+			}
+		});
+
+		Button addRowSecond = new Button("Add row to second list");
+		addRowSecond.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				second.push(new TextField());
+				update(firstListView, secondListView, tempListView, first, second, tempStack);
+			}
+		});
+
+		HBox buttonsLayout = new HBox();
+		buttonsLayout.getChildren().add(button);
+		buttonsLayout.getChildren().add(addRowFirst);
+		buttonsLayout.getChildren().add(addRowSecond);
+
+		borderPane.setBottom(buttonsLayout);
+
 		Label label = new Label("Задание 5\n" +
 				"Задать два стека, поменять информацию местами. Отображать процесс вычислений по нажатию кнопки с помощью элементов\n" +
 				"Vertical List View.");
@@ -92,5 +114,12 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.sizeToScene();
 		primaryStage.show();
+	}
+
+	static void update(ListView<TextField> firstListView, ListView<TextField> secondListView, ListView<TextField> tempListView,
+					   Stack<TextField> first, Stack<TextField> second, Stack<TextField> tempStack){
+		firstListView.getItems().setAll(first);    // обновляет данные в ListView
+		tempListView.getItems().setAll(tempStack);
+		secondListView.getItems().setAll(second);
 	}
 }
